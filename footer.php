c2fa6be68da81e01b010ff1d668b4b0745ea999f
unknown <sggurusabari@gmail.com>
2020-07-16 12:53:46 +0530

    guru

---
+++ footer.php
@@ -0,0 +1,13 @@
+<div class="wrapper col5">
+  <div id="footer">
+
+    <div id="copyright">
+      <p class="fl_left">Copyright &copy; 2018 - All Rights Reserved | <a href="adminlogin.php">Admin Login Panel</a> | <a href="doctorlogin.php">Doctor Login Panel</a></p>
+      <p class="fl_right">Developed by Gowseha</a></p>
+      <br class="clear" />
+    </div>
+    <div class="clear"></div>
+  </div>
+</div>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,81 @@
+<?php
+include("header.php");
+?>
+
+<div class="clear"> </div>
+  <!--start-image-slider---->
+      <div class="image-slider">
+        <!-- Slideshow 1 -->
+          <ul class="rslides" id="slider1">
+            <li><img src="images/slider-image1.jpg" alt=""></li>
+            <li><img src="images/slider-image2.jpg" alt=""></li>
+            <li><img src="images/slider-image1.jpg" alt=""></li>
+          </ul>
+         <!-- Slideshow 2 -->
+      </div>
+      <!--End-image-slider---->
+    <div class="clear"> </div>
+
+<div class="content-grids">
+  <div class="wrap">
+  <div class="section group">
+
+
+<div class="listview_1_of_3 images_1_of_3">
+  <div class="listimg listimg_1_of_2">
+      <img src="images/grid-img3.png">
+  </div>
+  <div class="text list_1_of_2">
+      <h3>Patients</h3>
+      <p>Register & Book Appointment</p>
+      <div class="button"><span><a href="patientlogin.php">Click Here</a></span></div>
+    </div>
+</div>
+
+<div class="listview_1_of_3 images_1_of_3">
+  <div class="listimg listimg_1_of_2">
+      <img src="images/grid-img1.png">
+  </div>
+  <div class="text list_1_of_2">
+      <h3>Doctors Login</h3>
+
+      <div class="button"><span><a href="doctorlogin.php">Click Here</a></span></div>
+  </div>
+</div>
+
+
+  <div class="listview_1_of_3 images_1_of_3">
+  <div class="listimg listimg_1_of_2">
+      <img src="images/grid-img2.png">
+  </div>
+  <div class="text list_1_of_2">
+      <h3>Admin Login</h3>
+
+      <div class="button"><span><a href="adminlogin.php">Click Here</a></span></div>
+     </div>
+</div>
+</div>
+</div>
+</div>
+
+<div class="wrap">
+<div class="content-box">
+<div class="section group">
+ <div class="col_1_of_3 span_1_of_3 frist">
+
+ </div>
+ <div class="col_1_of_3 span_1_of_3 second">
+
+ </div>
+ <div class="col_1_of_3 span_1_of_3 frist">
+
+
+ </div>
+</div>
+</div>
+</div>
+
+
+<?php
+include("footer.php");
+?>
